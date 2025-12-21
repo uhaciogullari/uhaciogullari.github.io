0a810afac9dd2d8db0ee7a651e22d8d4232442bb
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2025-12-21 01:39:45 +0100

    started eertree post

---
+++ assets/js/eertree-display.js
@@ -0,0 +1,92 @@
+﻿const display = (settings = {}) => {
+
+    const defaults = {
+        hideOddRoot: false,
+        hideEvenRoot: false,
+        hideSuffixLinks: false,
+        markMaxSuffix: true,
+        value: "",
+        inputId: ""
+    };
+
+    settings = { ...defaults, ...settings };
+
+    if (!document.getElementById(settings.targetId)){
+        return;
+    }
+
+    if(settings.value) {
+        render(settings.value, settings)
+    } else if(settings.inputId) {
+        const input = document.getElementById(settings.inputId);
+        input.addEventListener("input", () => {
+            render(input.value ||"", settings);
+        });
+        render(input.value || "", settings);
+    }
+}
+
+const isNodeHidden = (id, settings) => {
+    if (settings.hideOddRoot && id === 0) {
+        return true;
+    }
+    if (settings.hideEvenRoot && id === 1) {
+        return true;
+    }
+
+    return false;
+}
+
+const toGraphvizDot = (t, settings) => {
+    const digraph = ["digraph {", "ranksep = 1.2;", "nodesep = 0.8;", "beautify=true;"]
+    const nodes = []
+    const edges = []
+    t.nodes.forEach((node, index) => {
+        if (isNodeHidden(node.id, settings)) {
+            return;
+        }
+
+        const nodeLabel = node.palindrome !== "" ? node.palindrome : node.length;
+        const isSpecialNode = node.length <= 0;
+        const markMaxSuffix = settings.markMaxSuffix && t.maxSuffix == node;
+        nodes.push(`${node.id} [label="${nodeLabel}"] ${isSpecialNode ? "[shape = doublecircle]" : ""} ${markMaxSuffix ? " [color=red]" : ""};`)
+
+        if(!isNodeHidden(node.link.id, settings) && !settings.hideSuffixLinks) {
+            edges.push(`${node.id} -> ${node.link.id} [color = steelblue] [constraint = false]; `)
+        }
+
+        for (const [key, val] of node.edges) {
+            if (isNodeHidden(val.id, settings)) {
+                continue;
+            }
+            edges.push(`${node.id} -> ${val.id} [label="${key}"] [labeldistance = 1.25];`)
+        }
+    });
+    digraph.push(...nodes)
+    digraph.push(...edges)
+    digraph.push("}")
+    return digraph.join("\n")
+}
+
+
+
+const render = (value, settings) => {
+    const eertree = buildEertree(value);
+    const dot = toGraphvizDot(eertree, settings)
+
+    let viz = new Viz();
+    viz.renderSVGElement(dot).then(svg => {
+        let target = document.getElementById(settings.targetId);
+        target.innerHTML = "";
+        target.appendChild(svg);
+    });
+}
+
+const buildEertree= (s) => {
+    const eertree = new Eertree();
+    for (let c of s) {
+        eertree.add(c);
+    }
+    return eertree;
+}
+
---
+++ assets/js/eertree.js
@@ -0,0 +1,57 @@
+﻿class Node {
+    constructor(length, suffix, id) {
+        this.edges = new Map();
+        this.link = suffix;
+        this.length = length;
+        this.palindrome = "";
+        this.id = id;
+    }
+}
+
+class Eertree {
+    constructor() {
+        this.imaginary = new Node(-1, null, 0, 1);
+        this.imaginary.link = this.imaginary;
+        this.empty = new Node(0, this.imaginary, 1, 2);
+        this.maxSuffix = this.imaginary;
+        this.accumulated = "";
+        this.nodes = [this.imaginary, this.empty];
+    }
+
+    add(ch) {
+        const getMaxSuffixPalindrome = (startNode, c) => {
+            for (let current = startNode; current !== this.imaginary; current = current.link) {
+                const compare = this.accumulated.length - current.length - 1;
+                if (compare >= 0 && this.accumulated[compare] === c) {
+                    return current;
+                }
+            }
+            return this.imaginary;
+        };
+
+
+        let maxSuffix = getMaxSuffixPalindrome(this.maxSuffix, ch);
+        let createNewNode = !(maxSuffix.edges.has(ch));
+        if (createNewNode) {
+            let newNode = new Node(maxSuffix.length + 2, null, this.nodes.length);
+
+            if (newNode.length === 1) {
+                newNode.link = this.empty;
+                newNode.palindrome = ch;
+            } else {
+                newNode.link = getMaxSuffixPalindrome(maxSuffix.link, ch).edges.get(ch);
+                newNode.palindrome = ch + maxSuffix.palindrome + ch;
+            }
+            newNode.parent = maxSuffix;
+            maxSuffix.edges.set(ch, newNode);
+            this.nodes.push(newNode);
+        }
+
+        this.maxSuffix = maxSuffix.edges.get(ch);
+        this.accumulated += ch;
+
+        return createNewNode === true ? 1 : 0;
+    }
+
+
+}
